230262688ed4355754caf66e62220d8d672cb890
Blake Boswell <blake.w.boswell@gmail.com>
2022-11-25 00:32:14 -0600

    Added puzzle completion detection and separated reducer logic

--- src/pages/Puzzle.tsx
+++ src/pages/Puzzle.tsx
@@ -1,182 +1,37 @@
-import React, { Reducer, useEffect, useReducer, useState } from 'react';
+import React, {
+  Reducer,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from 'react';
 import { PuzzleState, PuzzleAction, PuzzlePiece } from '../types/puzzle';
-
-function puzzleReducer(state: PuzzleState, action: PuzzleAction) {
-  const { type, id } = action;
-  const { emptyLocation, pieces, rowSize, columnSize } = state;
-  const { row: emptyRow, column: emptyCol } = emptyLocation;
-  const rowMin = 0;
-  const rowMax = rowSize - 1;
-  const colMin = 0;
-  const colMax = columnSize - 1;
-  const targetPieceIndex = pieces.findIndex(piece => piece.id === id);
-  if (targetPieceIndex > -1) {
-    const { row, column } = pieces[targetPieceIndex];
-
-    switch (type) {
-      case 'move-up': {
-        // Check to see if move is legal
-        if (row === rowMin) return state;
-        if (!(row - 1 === emptyRow && column === emptyCol)) return state;
-
-        // Legal move
-        const newPieces = pieces.map(piece => {
-          if (piece.id === id) {
-            // Update row
-            return {
-              ...piece,
-              row: piece.row - 1,
-            };
-          } else if (piece.row === emptyRow && piece.column === emptyCol) {
-            // Update empty position row
-            return {
-              ...piece,
-              row: piece.row + 1,
-            };
-          }
-
-          return piece;
-        });
-
-        return {
-          ...state,
-          pieces: newPieces,
-          emptyLocation: {
-            row: emptyLocation.row + 1,
-            column: emptyLocation.column,
-          },
-        };
-      }
-      case 'move-down': {
-        // Check to see if move is legal
-        if (row === rowMax) return state;
-        if (!(row + 1 === emptyRow && column === emptyCol)) return state;
-
-        // Legal move
-        const newPieces = pieces.map(piece => {
-          if (piece.id === id) {
-            // Update row
-            return {
-              ...piece,
-              row: piece.row + 1,
-            };
-          } else if (piece.row === emptyRow && piece.column === emptyCol) {
-            // Update empty position row
-            return {
-              ...piece,
-              row: piece.row - 1,
-            };
-          }
-
-          return piece;
-        });
-
-        return {
-          ...state,
-          pieces: newPieces,
-          emptyLocation: {
-            row: emptyLocation.row - 1,
-            column: emptyLocation.column,
-          },
-        };
-      }
-      case 'move-left': {
-        // Check to see if move is legal
-        if (column === colMin) return state;
-        if (!(row === emptyRow && column - 1 === emptyCol)) return state;
-
-        // Legal move
-        const newPieces = pieces.map(piece => {
-          if (piece.id === id) {
-            // Update column
-            return {
-              ...piece,
-              column: piece.column - 1,
-            };
-          } else if (piece.row === emptyRow && piece.column === emptyCol) {
-            // Update empty position column
-            return {
-              ...piece,
-              column: piece.column + 1,
-            };
-          }
-
-          return piece;
-        });
-
-        return {
-          ...state,
-          pieces: newPieces,
-          emptyLocation: {
-            row: emptyLocation.row,
-            column: emptyLocation.column + 1,
-          },
-        };
-      }
-      case 'move-right': {
-        // Check to see if move is legal
-        if (column === colMax) return state;
-        if (!(row === emptyRow && column + 1 === emptyCol)) return state;
-
-        // Legal move
-        const newPieces = pieces.map(piece => {
-          if (piece.id === id) {
-            // Update column
-            return {
-              ...piece,
-              column: piece.column + 1,
-            };
-          } else if (piece.row === emptyRow && piece.column === emptyCol) {
-            // Update empty position column
-            return {
-              ...piece,
-              column: piece.column - 1,
-            };
-          }
-          return piece;
-        });
-
-        return {
-          ...state,
-          pieces: newPieces,
-          emptyLocation: {
-            row: emptyLocation.row,
-            column: emptyLocation.column - 1,
-          },
-        };
-      }
-      default:
-        return state;
-    }
-  }
-
-  return state;
-}
+import { scramble } from '../util/puzzleUtils';
+import { puzzleReducer } from './reducers/puzzleReducer';
 
 const Puzzle = () => {
   const [focusedPiece, setFocusedPiece] = useState(-1);
   const [puzzle, dispatchMove] = useReducer<Reducer<PuzzleState, PuzzleAction>>(
     puzzleReducer,
-    {
-      rowSize: 3,
-      columnSize: 3,
-      pieces: [
-        { id: 1, row: 0, column: 0 },
-        { id: 2, row: 0, column: 1 },
-        { id: 3, row: 0, column: 2 },
-        { id: 4, row: 1, column: 0 },
-        { id: 5, row: 1, column: 1 },
-        { id: 6, row: 1, column: 2 },
-        { id: 7, row: 2, column: 0 },
-        { id: 8, row: 2, column: 1 },
-        { id: 9, row: 2, column: 2 },
-      ],
-      emptyLocation: {
-        row: 2,
-        column: 2,
-      },
-    },
+    scramble(3, 3),
   );
+
+  const isPuzzleComplete = useMemo(() => {
+    for (let i = 0; i < puzzle.pieces.length - 1; i++) {
+      const thisPiece = puzzle.pieces[i];
+      const nextPiece = puzzle.pieces[i + 1];
+      if (thisPiece.row > nextPiece.row) {
+        return false;
+      } else if (
+        thisPiece.row === nextPiece.row &&
+        thisPiece.column > nextPiece.column
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  }, [puzzle.pieces]);
 
   useEffect(() => {
     if (focusedPiece > -1) {
@@ -332,6 +187,7 @@ const Puzzle = () => {
           ),
         )}
       </div>
+      <div>Puzzle status: {isPuzzleComplete ? 'Done' : 'In progress'}</div>
     </div>
   );
 };
---
+++ src/pages/reducers/puzzleReducer.ts
@@ -0,0 +1,153 @@
+import { PuzzleAction, PuzzleState } from '../../types/puzzle';
+
+export function puzzleReducer(state: PuzzleState, action: PuzzleAction) {
+  const { type, id } = action;
+  const { emptyLocation, pieces, rowSize, columnSize } = state;
+  const { row: emptyRow, column: emptyCol } = emptyLocation;
+  const rowMin = 0;
+  const rowMax = rowSize - 1;
+  const colMin = 0;
+  const colMax = columnSize - 1;
+  const targetPieceIndex = pieces.findIndex(piece => piece.id === id);
+  if (targetPieceIndex > -1) {
+    const { row, column } = pieces[targetPieceIndex];
+
+    switch (type) {
+      case 'move-up': {
+        // Check to see if move is legal
+        if (row === rowMin) return state;
+        if (!(row - 1 === emptyRow && column === emptyCol)) return state;
+
+        // Legal move
+        const newPieces = pieces.map(piece => {
+          if (piece.id === id) {
+            // Update row
+            return {
+              ...piece,
+              row: piece.row - 1,
+            };
+          } else if (piece.row === emptyRow && piece.column === emptyCol) {
+            // Update empty position row
+            return {
+              ...piece,
+              row: piece.row + 1,
+            };
+          }
+
+          return piece;
+        });
+
+        return {
+          ...state,
+          pieces: newPieces,
+          emptyLocation: {
+            row: emptyLocation.row + 1,
+            column: emptyLocation.column,
+          },
+        };
+      }
+      case 'move-down': {
+        // Check to see if move is legal
+        if (row === rowMax) return state;
+        if (!(row + 1 === emptyRow && column === emptyCol)) return state;
+
+        // Legal move
+        const newPieces = pieces.map(piece => {
+          if (piece.id === id) {
+            // Update row
+            return {
+              ...piece,
+              row: piece.row + 1,
+            };
+          } else if (piece.row === emptyRow && piece.column === emptyCol) {
+            // Update empty position row
+            return {
+              ...piece,
+              row: piece.row - 1,
+            };
+          }
+
+          return piece;
+        });
+
+        return {
+          ...state,
+          pieces: newPieces,
+          emptyLocation: {
+            row: emptyLocation.row - 1,
+            column: emptyLocation.column,
+          },
+        };
+      }
+      case 'move-left': {
+        // Check to see if move is legal
+        if (column === colMin) return state;
+        if (!(row === emptyRow && column - 1 === emptyCol)) return state;
+
+        // Legal move
+        const newPieces = pieces.map(piece => {
+          if (piece.id === id) {
+            // Update column
+            return {
+              ...piece,
+              column: piece.column - 1,
+            };
+          } else if (piece.row === emptyRow && piece.column === emptyCol) {
+            // Update empty position column
+            return {
+              ...piece,
+              column: piece.column + 1,
+            };
+          }
+
+          return piece;
+        });
+
+        return {
+          ...state,
+          pieces: newPieces,
+          emptyLocation: {
+            row: emptyLocation.row,
+            column: emptyLocation.column + 1,
+          },
+        };
+      }
+      case 'move-right': {
+        // Check to see if move is legal
+        if (column === colMax) return state;
+        if (!(row === emptyRow && column + 1 === emptyCol)) return state;
+
+        // Legal move
+        const newPieces = pieces.map(piece => {
+          if (piece.id === id) {
+            // Update column
+            return {
+              ...piece,
+              column: piece.column + 1,
+            };
+          } else if (piece.row === emptyRow && piece.column === emptyCol) {
+            // Update empty position column
+            return {
+              ...piece,
+              column: piece.column - 1,
+            };
+          }
+          return piece;
+        });
+
+        return {
+          ...state,
+          pieces: newPieces,
+          emptyLocation: {
+            row: emptyLocation.row,
+            column: emptyLocation.column - 1,
+          },
+        };
+      }
+      default:
+        return state;
+    }
+  }
+
+  return state;
+}
